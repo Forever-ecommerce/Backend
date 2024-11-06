01e4e6ff511e56891a95cd7d8c37269e30688474
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-11-06 10:21:20 +0530

    created

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import connectDB from "./config/miongodb.js";
 // import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 //App Config
 
@@ -19,8 +21,10 @@ app.use(express.json());
 app.use(cors());
 
 //API endpoints
-app.use('/api/user',userRouter);
-app.use('/api/product',productRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
